5c2099d4287cd76a79532149af0438cefc5dae50
Anton Patron <anton.patron@hogia.se>
2022-12-09 00:10:12 +0100

    Completed day 8

--- Common/Parsing/ParserCreator.cs
+++ Common/Parsing/ParserCreator.cs
@@ -1,7 +1,10 @@
-﻿namespace Common.Parsing.Interfaces;
+﻿using Common.Parsing.Interfaces;
+
+namespace Common.Parsing;
 
 public static class ParserCreator
 {
     public static Int32Parser Int32Parser = new Int32Parser();
     public static StringParser StringParser = new StringParser();
+    public static Int32Parser2D Int32Parser2D = new Int32Parser2D();
 }
---
+++ Day8/Program.cs
@@ -0,0 +1,47 @@
+﻿using Common.Parsing;
+using Day8;
+
+Console.WriteLine("Day 8");
+Console.WriteLine();
+Console.WriteLine("Part 1");
+Console.WriteLine();
+
+string inputFile = @"Day8.Input.txt";
+
+var input = await ParseHelper.GetInput(inputFile);
+
+var textRows = input.Parse<string>(new string[] { Environment.NewLine },
+                                 ParserCreator.StringParser);
+
+var array = new int[textRows.Length, textRows[0].Count()];
+for (int y = 0; y < textRows.Length; y++)
+{
+    var line = textRows[y];
+    var rows = line.Select(charItem => charItem.ToString()).Select(number => ParseHelper.Parse(ParserCreator.Int32Parser, number)).ToArray();
+    for (int x = 0; x < rows.Count(); x++)
+        array[y, x] = rows[x];
+}
+
+List<Tree> trees = new List<Tree>();
+for (int y = 0; y < array.GetLength(0); y++)
+{
+    for (int x = 0; x < array.GetLength(1); x++)
+    {
+        var height = array[y, x];
+
+        var tree = new Tree(height, new Point(x, y), array.GetLength(1) - 1, array.GetLength(0) - 1);
+        trees.Add(tree);
+    }
+}
+
+var visableTrees = trees.Where(x => x.IsVisible(trees)).ToArray();
+
+Console.WriteLine(visableTrees.Count());
+
+Console.WriteLine();
+Console.WriteLine("Part 2");
+Console.WriteLine();
+
+var highestScenic = trees.Max(x => x.GetScenicScore(trees));
+
+Console.WriteLine(highestScenic);
