b22354bc25be18b9485932e20081a640da51c476
Anton Patron <anton.patron@hogia.se>
2023-01-11 23:46:03 +0100

    Completed day 12

---
+++ Day12/GridParser.cs
@@ -0,0 +1,38 @@
+﻿using Common.Parsing;
+using Common.Parsing.Interfaces;
+
+namespace Day12
+{
+    public sealed class GridParser : IParser<Grid>
+    {
+        Grid IParser<Grid>.Parse(string value)
+        {
+            List<string> elevations = new List<string>
+            {
+                "S","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z", "E"
+            };
+            var rows = ParseHelper.Parse(value, new string[] { Environment.NewLine }, ParserCreator.StringParser);
+
+            var positions = rows.Select((row, y) =>
+            {
+                return row
+                .Select((letter, x) =>
+                {
+                    var elevation = elevations.IndexOf(letter.ToString().Trim());
+                    if (elevation < 0)
+                        throw new Exception();
+
+                    if (letter.ToString() == "E")
+                        return new Position(new Coordinate(y, x), elevation, Destination: true);
+                    else if (letter.ToString() == "S")
+                        return new Position(new Coordinate(y, x), elevation, Starting: true);
+
+                    return new Position(new Coordinate(y, x), elevation);
+                });
+            }).SelectMany(pos => pos.ToList()).ToList();
+
+            return new Grid(positions);
+        }
+    }
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,83 @@
+﻿using Common.Parsing;
+using Day12;
+
+Console.WriteLine("Day 12");
+Console.WriteLine();
+
+string inputFile = @"Day12.Input.txt";
+var input = await ParseHelper.GetInput(inputFile);
+
+var grid = ParseHelper.Parse(new GridParser(), input);
+
+var stepsPart1 = BFS(grid, new GridItem(grid.Positions.Single(pos => pos.Starting)));
+Console.WriteLine("Part 1:");
+Console.WriteLine(stepsPart1);
+Console.WriteLine();
+
+Console.WriteLine("Part 2:");
+var stepsPart2 = grid.Positions.Where(pos => pos.Elevation == 1).Select(pos => BFS(grid, new GridItem(pos))).Where(res => res != null).Min();
+Console.WriteLine(stepsPart2);
+
+int? BFS(Grid grid, GridItem currentItem)
+{
+    var visited = new List<GridItem>();
+    var queue = new Queue<GridItem>();
+    GridItem? previousItem = null;
+    int steps = 0;
+    queue.Enqueue(currentItem);
+    int iteration = currentItem.Iteration;
+    while (queue.Count > 0)
+    {
+        currentItem = queue.Dequeue();
+
+        if (iteration != currentItem.Iteration)
+        {
+            iteration = currentItem.Iteration;
+            steps++;
+        }
+
+        visited.Add(currentItem);
+
+        var neigbhours = currentItem.GetNeighbours(previousItem?.Position, grid);
+        foreach (Position neighbor in neigbhours)
+        {
+            if (neighbor.Destination)
+                return ++steps;
+
+            if (!visited.Any(vis => vis.Position == neighbor) && !queue.Any(x => x.Position == neighbor))
+            {
+                var enquedItem = new GridItem(neighbor) { Iteration = iteration + 1 };
+                queue.Enqueue(enquedItem);
+            }
+        }
+
+        previousItem = currentItem;
+    }
+
+    return null;
+}
+
+record GridItem
+{
+    public GridItem(Position position)
+    {
+        Position = position;
+    }
+
+    public Position Position { get; }
+
+    public int Iteration { get; init; }
+    public IEnumerable<Position> GetNeighbours(Position? previous, Grid grid)
+    {
+        var neigbhours = grid.Positions.Where(pos => previous != pos && ((pos.Coordinate.X == Position.Coordinate.X && (pos.Coordinate.Y == Position.Coordinate.Y + 1 || pos.Coordinate.Y == Position.Coordinate.Y - 1))
+                                                  || (pos.Coordinate.Y == Position.Coordinate.Y && (pos.Coordinate.X == Position.Coordinate.X + 1 || pos.Coordinate.X == Position.Coordinate.X - 1)))).ToList();
+
+        return neigbhours.Where(pos => ((pos.Elevation <= Position.Elevation) || (pos.Elevation == Position.Elevation + 1))).ToList();
+    }
+}
+
+record struct Coordinate(int Y, int X);
+
+record Position(Coordinate Coordinate, int Elevation, bool Starting = false, bool Destination = false);
+
+record Grid(IEnumerable<Position> Positions);
