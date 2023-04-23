3b5786ff38e2983da66fc15d0ac72d522bd8388f
Anton Patron <anton.patron@hogia.se>
2023-04-23 14:20:08 +0200

    Completed day 13

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Immutable;
 using Common.Parsing;
 using Day13;
 
@@ -8,122 +9,159 @@ Console.WriteLine("Day 13");
 string inputFile = @"Day13.Input.txt";
 var input = await ParseHelper.GetInput(inputFile);
 
-var pairs = ParseHelper.Parse(new PairParser(), input);
+var rows = ParseHelper.Parse(new RowParser(), input);
+var indexes = new List<int>();
+Part1(indexes, rows, 0);
 
-var rightOrders = pairs.Where(pair => pair.RightOrder()).ToList();
 Console.WriteLine("Part one");
-Console.WriteLine(rightOrders.Sum(pair => pair.Index));
+Console.WriteLine(indexes.Sum());
 
-public record Pair(int Index, ListPart PartOne, ListPart PartTwo)
+Console.WriteLine();
+
+Console.WriteLine("Part two");
+Console.WriteLine(Part2(rows));
+
+void Part1(List<int> indexes, List<List<IPart>> partsList, int skipper)
 {
-    public bool RightOrder()
+    if (skipper >= partsList.Count())
+        return;
+
+    var parts = partsList.Skip(skipper).Take(2);
+    if (Comparer(parts.First(), parts.Last()) == Result.Left)
+        indexes.Add((skipper + 2) / 2);
+
+    skipper += 2;
+    Part1(indexes, partsList, skipper);
+}
+
+int Part2(List<List<IPart>> partsList)
+{
+    List<IPart> decoder = new List<IPart>();
+    decoder.Add(new StartPart());
+    decoder.Add(new IntPart(2, true));
+    decoder.Add(new EndPart());
+
+    List<IPart> decoder2 = new List<IPart>();
+    decoder2.Add(new StartPart());
+    decoder2.Add(new IntPart(6, true));
+    decoder2.Add(new EndPart());
+
+    partsList.Add(decoder);
+    partsList.Add(decoder2);
+
+    List<List<IPart>> orderdList = new List<List<IPart>>();
+    foreach (var partList in partsList)
     {
-        var result = Check(PartOne.Move(), PartTwo.Move());
-        if (result == Result.Equal)
-            throw new Exception();
-
-        return result == Result.Left;
-
-        static Result Check(ListPart? listPartOne, ListPart? listPartTwo)
+        if (!orderdList.Any())
         {
-            Result result = Result.Equal;
-            while (result == Result.Equal)
-            {
-                switch (listPartOne, listPartTwo)
+            orderdList.Add(partList);
+            continue;
+        }
+        int index = 0;
+        foreach (var orderdPartList in orderdList)
+        {
+            var result = Comparer(partList, orderdPartList);
+            if (result == Result.Equal)
+                throw new Exception("This should not be able to happend.");
+
+            if (result == Result.Left)
+                break;
+
+            index++;
+        }
+        orderdList.Insert(index, partList);
+    }
+
+    List<int> indexes = new List<int>();
+    for (int index = 0; index < orderdList.Count(); index++)
+    {
+        if (orderdList[index].Any(part => part is IntPart && ((IntPart)part).Marker))
+            indexes.Add(index + 1);
+    }
+
+    return indexes.Aggregate(1, (a, b) => a * b);
+}
+
+Result Comparer(IEnumerable<IPart> partsOne, IEnumerable<IPart> partsTwo)
+{
+    var mover = new Mover(partsOne.ToImmutableList());
+    var mover2 = new Mover(partsTwo.ToImmutableList());
+
+    return GetResult(mover, mover2);
+
+    Result GetResult(Mover moverOne, Mover moverTwo)
+    {
+        var part = moverOne.GetPart();
+        var partTwo = moverTwo.GetPart();
+        switch (part, partTwo)
+        {
+            case (null, null):
+                return Result.Equal;
+
+            case (StartPart _, StartPart _):
+            case (EndPart _, EndPart _):
+            case (IntPart intPart, IntPart intPartTwo) when intPart.Value == intPartTwo.Value:
                 {
-                    case (null, null):
+                    moverOne.Move();
+                    moverTwo.Move();
 
-                        return Result.Equal;
-
-                    case (null, _):
-                        {
-                            return Result.Left;
-                        }
-
-                    case (_, null):
-                        {
-                            return Result.Right;
-                        }
+                    return GetResult(moverOne, moverTwo);
                 }
 
-                var partOne = listPartOne.GetPart();
-                var partTwo = listPartTwo.GetPart();
+            case (IntPart intPart, StartPart _):
+                moverOne = moverOne.Insert(intPart.Value);
 
-                switch (partOne, partTwo)
-                {
-                    case (IntPart intPart, IntPart intPartTwo):
-                        {
-                            if (intPart.Value == intPartTwo.Value)
-                            {
-                                result = Result.Equal;
-                                listPartOne = listPartOne.Move();
-                                listPartTwo = listPartTwo.Move();
-                            }
-                            else if (intPart.Value < intPartTwo.Value)
-                                result = Result.Left;
-                            else
-                                result = Result.Right;
+                return GetResult(moverOne, moverTwo);
 
-                            break;
-                        }
+            case (StartPart _, IntPart intPart):
+                moverTwo = moverTwo.Insert(intPart.Value);
 
-                    case (ListPart nextListPart, ListPart nextlistPartTwo):
-                        {
-                            result = Check(nextListPart.Move(), nextlistPartTwo.Move());
-                            break;
-                        }
+                return GetResult(moverOne, moverTwo);
 
-                    case (IntPart intPart, ListPart nextlistPartTwo):
-                        {
-                            var newListPart = new ListPart(new List<IPart> { intPart }, listPartOne);
-                            listPartOne.Insert(newListPart);
-                            result = Check(newListPart.Move(), nextlistPartTwo.Move());
-                            break;
-                        }
+            case (EndPart, _):
+            case (null, _):
+            case (IntPart intPart, IntPart intPartTwo) when intPart.Value < intPartTwo.Value:
+                return Result.Left;
 
-                    case (ListPart nextListPart, IntPart intPartTwo):
-                        {
-                            var newListPart = new ListPart(new List<IPart> { intPartTwo }, listPartTwo);
-                            listPartTwo.Insert(newListPart);
-                            return Check(nextListPart.Move(), newListPart.Move());
-                        }
+            case (_, EndPart):
+            case (_, null):
+            case (IntPart intPart, IntPart intPartTwo) when intPart.Value > intPartTwo.Value:
+                return Result.Right;
 
-                    default:
-                        throw new Exception("Missing mapping");
-                }
-            }
-
-            return result;
+            default: throw new ArgumentException();
         }
     }
 }
 
-public record ListPart(List<IPart> Parts, ListPart? ParentListPart) : IPart
+record Mover(ImmutableList<IPart> Parts)
 {
-    private int Index { get; set; } = -1;
-
-    public ListPart? Move()
+    private int _index;
+    public void Move()
     {
-        this.Index++;
-        if (this.Index == this.Parts.Count())
-            return ParentListPart?.Move() ?? null;
-
-        return this;
+        ++_index;
+    }
+    public IPart? GetPart()
+    {
+        return Parts.Count() > _index ? Parts[_index] : null;
     }
 
-    public void Insert(ListPart part)
+    public Mover Insert(int number)
     {
-        this.Parts[Index] = part;
-    }
+        var newList = this.Parts.ToList();
+        newList[_index] = new StartPart();
+        newList.Insert(_index + 1, new IntPart(number));
+        newList.Insert(_index + 2, new EndPart());
 
-    public IPart GetPart() => this.Parts[Index];
+        return this with { Parts = newList.ToImmutableList() };
+    }
 }
 
-public record IntPart(int Value) : IPart;
+public record struct IntPart(int Value, bool Marker = false) : IPart;
+public record struct EndPart() : IPart;
+public record struct StartPart() : IPart;
 
 public interface IPart
-{
-}
+{ }
 
 public enum Result
 {
---
+++ Day13/RowParser.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using Common.Parsing;
+using Common.Parsing.Interfaces;
+
+namespace Day13
+{
+    public sealed class RowParser : IParser<List<List<IPart>>>
+    {
+        List<List<IPart>> IParser<List<List<IPart>>>.Parse(string value)
+        {
+            var rows = ParseHelper.Parse(value, new string[] { Environment.NewLine }, ParserCreator.StringParser, StringSplitOptions.RemoveEmptyEntries);
+            int taker = 0;
+            var list = new List<List<IPart>>();
+            while (taker < rows.Length)
+            {
+                var pairRows = rows.Skip(taker).Take(1);
+                var firstPart = GetListRow(pairRows.First());
+
+                Console.WriteLine();
+
+                list.Add(firstPart);
+                taker += 1;
+            }
+
+            return list;
+        }
+
+        protected List<IPart> GetListRow(string content)
+        {
+            StringBuilder builder = new StringBuilder();
+            var rootList = new List<IPart>();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            List<IPart> list = new List<IPart>();
+            TakeWhile(list, content, stringBuilder);
+
+            Console.WriteLine(stringBuilder.ToString());
+
+            return list;
+
+            void TakeWhile(List<IPart> list, IEnumerable<char> content, StringBuilder builder)
+            {
+                char? part = content.Take(1).FirstOrDefault();
+                switch (part)
+                {
+                    case ',':
+                        builder.Append(",");
+                        TakeWhile(list, content.Skip(1), builder);
+                        break;
+
+                    case '[':
+                        builder.Append('[');
+                        list.Add(new StartPart());
+                        TakeWhile(list, content.Skip(1), builder);
+                        break;
+
+                    case ']':
+                        builder.Append(']');
+                        list.Add(new EndPart());
+                        TakeWhile(list, content.Skip(1), builder);
+                        break;
+
+                    case null:
+                        builder.Append("End");
+                        break;
+
+                    default:
+                        var charNumbers = content.TakeWhile(ch => int.TryParse(ch.ToString(), out int _)).ToList();
+                        if (!charNumbers.Any())
+                            return;
+
+                        var intNumber = ParserCreator.Int32Parser.Parse(string.Join("", charNumbers));
+                        list.Add(new IntPart(intNumber));
+                        builder.Append(intNumber);
+                        TakeWhile(list, content.Skip(charNumbers.Count()), builder);
+                        break;
+                }
+            }
+        }
+    }
+}
